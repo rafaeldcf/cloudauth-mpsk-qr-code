b65e2f502a882c054f61fdc1b974550c6512a515
Rafael del Cerro <4015219+rafaeldcf@users.noreply.github.com>
2024-06-22 09:16:48 +0200

    User feedback for active menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,13 +2,16 @@ import { NavLink } from "@mantine/core";
 import Link from "next/link";
 import React from "react";
 import { IconSettings, IconUser, IconUserPlus } from "@tabler/icons-react";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <>
-      <NavLink href="/config" label="Aruba Central Tokens" leftSection={<IconSettings size="1rem" stroke={1.5} />} component={Link} />
-      <NavLink href="/users" label="Users" leftSection={<IconUser size="1rem" stroke={1.5} />} component={Link} />
-      <NavLink href="/new" label="New User" leftSection={<IconUserPlus size="1rem" stroke={1.5} />} component={Link} />
+      <NavLink href="/config" label="Aruba Central Tokens" leftSection={<IconSettings size="1rem" stroke={1.5} />} component={Link} active={pathname == "/config"} />
+      <NavLink href="/users" label="Users" leftSection={<IconUser size="1rem" stroke={1.5} />} component={Link} active={pathname == "/users"} />
+      <NavLink href="/new" label="New User" leftSection={<IconUserPlus size="1rem" stroke={1.5} />} component={Link} active={pathname == "/new"} />
     </>
   );
 }
