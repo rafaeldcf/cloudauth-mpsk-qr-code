3f4496a9a46ea9bada886e4b7409c21810a376af
Rafael del Cerro <4015219+rafaeldcf@users.noreply.github.com>
2024-06-29 22:40:46 +0200

    Link to Github in the header

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -1,24 +1,32 @@
 "use client";
 import React, { useState } from "react";
 
-import { Anchor, AppShell, Center, Group, Text } from "@mantine/core";
+import { ActionIcon, Anchor, AppShell, Center, Group, Text } from "@mantine/core";
 import { MantineLogo } from "@mantinex/mantine-logo";
 
 import Navbar from "./Navbar";
-import { IconQrcode } from "@tabler/icons-react";
+import { IconBrandGithub, IconBrandGithubFilled, IconQrcode } from "@tabler/icons-react";
 import Link from "next/link";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <AppShell header={{ height: 60 }} navbar={{ width: 300, breakpoint: "sm" }} padding="md">
       <AppShell.Header>
-        <Group h="100%" px="md">
+        <Group h="100%" px="md" justify="space-between">
           <Anchor href="/" underline="never" component={Link}>
             <Center inline>
               <IconQrcode size={30} color="#228be6" />
               <Text fw={700} ml="xs" c="dark">
                 CloudAuth MPSK QR Code
               </Text>
+            </Center>
+          </Anchor>
+          <Anchor underline="never" href="https://github.com/rafaeldcf/cloudauth-mpsk-qr-code" target="_blank">
+            <Center color="none">
+              <ActionIcon variant="outline" radius="xl" aria-label="Settings" mr="xs">
+                <IconBrandGithub size={20} stroke={1.8} />{" "}
+              </ActionIcon>
+              Github Repository
             </Center>
           </Anchor>
         </Group>
