d987c0a2c9685dfbcfd0b318f4da346e600b8110
Rafael del Cerro <4015219+rafaeldcf@users.noreply.github.com>
2024-06-29 22:54:14 +0200

    Removing old dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { useQuery, useQueryClient, QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import CreateUser from "@/components/CreateUser";
-import { AppShell, Burger, Button, Grid, Group, Skeleton, TextInput } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-import { MantineLogo } from "@mantinex/mantine-logo";
-
-import ManageCentralToken from "@/components/ManageCentralToken";
-import Navbar from "@/components/Navbar";
-import MainLayout from "@/components/MainLayout";
-
-const queryClient = new QueryClient();
 
 export default function Home() {
   return (
